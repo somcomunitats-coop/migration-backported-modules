aab5287f96c8c39db2274d4902e349f7fe933470
Álvaro García <alvarogruiz8@gmail.com>
2024-10-31 11:44:27 +0100

    [IMP] Comments views

--- l10n_es_cooperator/__manifest__.py
+++ l10n_es_cooperator/__manifest__.py
@@ -1,7 +1,11 @@
 {
     "name": "Cooperator Spain Localisation",
     "version": "16.0.1.0.0",
-    "depends": ["cooperator", "cooperator_website", "l10n_es"],
+    "depends": [
+		"cooperator",
+		#"cooperator_website", 
+		"l10n_es"
+	       ],
     "author": "Odoo Community Association (OCA), Coop IT Easy SC, Coopdevs Treball SCCL",
     "mantainer": "Coopdevs Treball SCCL",
     "website": "https://github.com/OCA/cooperative",
@@ -11,8 +15,8 @@
     """,
     "license": "AGPL-3",
     "data": [
-        "views/subscription_request_view.xml",
-        "views/subscription_templates.xml",
+        #"views/subscription_request_view.xml",
+        #"views/subscription_templates.xml",
     ],
     "installable": True,
     "auto-install": True,
